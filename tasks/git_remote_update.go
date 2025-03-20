79f45151ff588133b2df9eb2ed058665ebf20103
Ying Huang <huangyingw@gmail.com>
2025-03-19 17:54:13 -0700

    n

--- tasks/git_remote_update.go
+++ tasks/git_remote_update.go
@@ -1,10 +1,16 @@
 package tasks
 
 import (
+	"fmt"
 	"os/exec"
 )
 
 func GitRemoteUpdate() error {
 	cmd := exec.Command("git", "remote", "update")
-	return cmd.Run()
+	err := cmd.Run()
+	if err != nil {
+		fmt.Printf("警告: git remote update 失败: %v\n", err)
+		return nil
+	}
+	return nil
 }
